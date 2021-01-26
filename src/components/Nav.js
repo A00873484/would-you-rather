73b9f84afbf1f0b43c2146fb062102b0699e6d5d
Daniel Engelhard <danielengelhard@Daniels-MacBook-Pro.local>
2021-01-26 14:00:49 -0800

    Logout added

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,7 @@ import { NavLink, withRouter } from 'react-router-dom'
 import { setAuthedUser } from '../actions/authedUser'
 import { connect } from 'react-redux'
 
-function Nav ({history, dispatch}) {
+function Nav ({history, dispatch, user}) {
     const path = history.location.pathname
     const selected = {color:'white'}
     const [logout, setLogout] = useState(false)
@@ -12,49 +12,61 @@ function Nav ({history, dispatch}) {
     const [leaderboard, setLeaderboard] = useState(false)
 
     const handleLogout = () => {
-        console.log('handleLogout')
         dispatch(setAuthedUser(null))
     }
+
+    console.log(user)
 
     return (
         <nav className='nav'>
             <ul className="menu">
                 <li onMouseEnter={() => setHome(true)}
                     onMouseLeave={() => setHome(false)}
-                    className={path === '/' || home?'trapazoid':''}>
+                    className={'vertical-center '+(path === '/' || home?'trapazoid':'')}>
                     <NavLink to='/' exact activeClassName='active' className="menu-tab" style={path === '/' || home ? selected : {}}>
                         Home
                     </NavLink>
                 </li>
                 <li onMouseEnter={() => setNewQuestion(true)}
                     onMouseLeave={() => setNewQuestion(false)}
-                    className={path === '/question' || newQuestion?'trapazoid':''} >
+                    className={'vertical-center '+(path === '/question' || newQuestion?'trapazoid':'')} >
                     <NavLink to='/question' activeClassName='active' className="menu-tab" style={path === "/question" || newQuestion ? selected : {}}>
                         New Question
                     </NavLink>
                 </li>
                 <li onMouseEnter={() => setLeaderboard(true)}
                     onMouseLeave={() => setLeaderboard(false)}
-                    className={path === '/leaderboard' || leaderboard?'trapazoid':''} >
+                    className={'vertical-center '+(path === '/leaderboard' || leaderboard?'trapazoid':'')} >
                     <NavLink to='/leaderboard' activeClassName='active' className="menu-tab" style={path === '/leaderboard' || leaderboard ? selected : {}}>
                         Leader Board
                     </NavLink>
                 </li>
             </ul>
-            <ul className="menu" style={{justifyContent:'flex-end', flexGrow:'1'}}>
-                <li className=""></li>
-                <li onMouseEnter={() => setLogout(true)}
-                    onMouseLeave={() => setLogout(false)}
-                    className={ logout ? 'trapazoid' : '' }>
-                        <span className="menu-tab"
-                            style={ logout ? selected : {}}
-                            onClick={handleLogout}>
-                                Logout
-                        </span>
-                </li>
-            </ul>
+            {
+                user && <ul className="menu" style={{justifyContent:'flex-end', flexGrow:'1'}}>
+                    <li className='vertical-center'>Hello, {user.name}</li>
+                    <li className='vertical-center'><img src={user.avatarURL}
+                        alt={`Avatar of ${user.name}`}
+                        className="mini-avatar"/></li>
+                    <li onMouseEnter={() => setLogout(true)}
+                        onMouseLeave={() => setLogout(false)}
+                        className={ 'vertical-center '+ (logout ? 'trapazoid' : '') }>
+                            <span className="menu-tab"
+                                style={ logout ? selected : {}}
+                                onClick={handleLogout}>
+                                    Logout
+                            </span>
+                    </li>
+                </ul>
+            }
         </nav>
     )
 }
 
-export default withRouter(connect()(Nav))
+function mapStateToProps ({ authedUser, users }) {
+    return {
+      user : users[authedUser]
+    }
+}
+
+export default withRouter(connect(mapStateToProps)(Nav))
